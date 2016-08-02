48d4cd694aa04f826a3570468f0ffce331663ca9
Anthony Simpson <ansi6622@colorado.edu>
2016-08-02 15:55:42 -0600

    rendering sql data in the form of json

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Anthony T. Simpson - Full Stack Engineer' });
+var knex = require('knex')(require('../knexfile')['development']);
+/* GET home/data page. */
+router.get('/data', function(req, res, next) {
+  knex('projects')
+    .then(function(projects) {
+      res.json({
+        title: 'Anthony T. Simpson - Full Stack Engineer',
+        projects: projects
+      });
+    })
 });
+
 module.exports = router;
