7ba07cc1345b40f332c063e7553e0aca554046a0
Anthony Simpson <ansi6622@colorado.edu>
2016-08-20 22:40:35 -0700

    added all the sites, display with angular, tray tables up said the captain so i gotta stop now

--- server/seeds/round_one.js
+++ server/seeds/round_one.js
@@ -7,9 +7,30 @@ exports.seed = function(knex, Promise) {
         // Inserts seed entries
         knex('projects').insert({
           name: 'EasySpeak',
-          description: 'Augmentative speech app for persons with disabilities/difficulties speaking',
+          description: 'Augmentative speech app for persons with speaking deficiencies and disabilities.',
           url: 'easy-speak.herokuapp.com/dashboard',
           github: 'github.com/ansi6622/q3-react-starter',
+          image_link: 'http://media.thecelebrityauction.co/picture/c/0c/CwMdBE5KXRIWEBcRB00ZEBAVDB4KCwYFFloAGARbBAoeHBUMHhwrBQRGRVpXQUdXVUJdWlJDXURNXEJCXVBTSh0OEEYcABECOggC/FQBbChoIExFLGAMCPBUAGkwTDAYdExMHDBUXFlsCBR0dFwkXLFNQR0pAW0FZRUddQ0dLCQIC.jpg'
+        }),
+        knex('projects').insert({
+          name: 'trender.io',
+          description: 'Data visualization for sentiment analysis on tweets realtime from the twitter streaming api.',
+          url: 'trendster-io.herokuapp.com',
+          github: 'github.com/ansi6622/Trender',
+          image_link: 'http://media.thecelebrityauction.co/picture/c/0c/CwMdBE5KXRIWEBcRB00ZEBAVDB4KCwYFFloAGARbBAoeHBUMHhwrBQRGRVpXQUdXVUJdWlJDXURNXEJCXVBTSh0OEEYcABECOggC/FQBbChoIExFLGAMCPBUAGkwTDAYdExMHDBUXFlsCBR0dFwkXLFNQR0pAW0FZRUddQ0dLCQIC.jpg'
+        }),
+        knex('projects').insert({
+          name: 'Stocker',
+          description: 'US stock trade application that uses machine learning models to predict closing prices.',
+          url: 'dragongiblets.herokuapp.com',
+          github: 'github.com/ansi6622/dragonjiblets',
+          image_link: 'http://media.thecelebrityauction.co/picture/c/0c/CwMdBE5KXRIWEBcRB00ZEBAVDB4KCwYFFloAGARbBAoeHBUMHhwrBQRGRVpXQUdXVUJdWlJDXURNXEJCXVBTSh0OEEYcABECOggC/FQBbChoIExFLGAMCPBUAGkwTDAYdExMHDBUXFlsCBR0dFwkXLFNQR0pAW0FZRUddQ0dLCQIC.jpg'
+        }),
+        knex('projects').insert({
+          name: 'GeoCatch',
+          description: 'Geolocation app that uses the IP Lookup API to identify hacker space locations and plot server coordinates on cesium.js globe.',
+          url: 'site-globe.herokuapp.com',
+          github: 'github.com/ansi6622/site-globe',
           image_link: 'http://media.thecelebrityauction.co/picture/c/0c/CwMdBE5KXRIWEBcRB00ZEBAVDB4KCwYFFloAGARbBAoeHBUMHhwrBQRGRVpXQUdXVUJdWlJDXURNXEJCXVBTSh0OEEYcABECOggC/FQBbChoIExFLGAMCPBUAGkwTDAYdExMHDBUXFlsCBR0dFwkXLFNQR0pAW0FZRUddQ0dLCQIC.jpg'
         }),
       ]);
