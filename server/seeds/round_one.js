27221c7fbe3f55ed6eeeb85556210092c7161ccd
Anthony Simpson <ansi6622@colorado.edu>
2016-08-23 20:01:13 -0600

    added skeleton css boilerplate, styled a bit, my links are being overidden by angular, placecage is fantastic, ng-src for img tags, and comments need some love but are lookin good, maybe i should also include sass

--- server/seeds/round_one.js
+++ server/seeds/round_one.js
@@ -10,28 +10,28 @@ exports.seed = function(knex, Promise) {
           description: 'Augmentative speech app for persons with speaking deficiencies and disabilities.',
           url: 'easy-speak.herokuapp.com/dashboard',
           github: 'github.com/ansi6622/q3-react-starter',
-          image_link: 'http://media.thecelebrityauction.co/picture/c/0c/CwMdBE5KXRIWEBcRB00ZEBAVDB4KCwYFFloAGARbBAoeHBUMHhwrBQRGRVpXQUdXVUJdWlJDXURNXEJCXVBTSh0OEEYcABECOggC/FQBbChoIExFLGAMCPBUAGkwTDAYdExMHDBUXFlsCBR0dFwkXLFNQR0pAW0FZRUddQ0dLCQIC.jpg'
+          image_link: 'http://www.placecage.com/gif/199/300'
         }),
         knex('projects').insert({
           name: 'trender.io',
           description: 'Data visualization for sentiment analysis on tweets realtime from the twitter streaming api.',
           url: 'trendster-io.herokuapp.com',
           github: 'github.com/ansi6622/Trender',
-          image_link: 'http://media.thecelebrityauction.co/picture/c/0c/CwMdBE5KXRIWEBcRB00ZEBAVDB4KCwYFFloAGARbBAoeHBUMHhwrBQRGRVpXQUdXVUJdWlJDXURNXEJCXVBTSh0OEEYcABECOggC/FQBbChoIExFLGAMCPBUAGkwTDAYdExMHDBUXFlsCBR0dFwkXLFNQR0pAW0FZRUddQ0dLCQIC.jpg'
+          image_link: 'http://www.placecage.com/gif/200/301'
         }),
         knex('projects').insert({
           name: 'Stocker',
           description: 'US stock trade application that uses machine learning models to predict closing prices.',
           url: 'dragongiblets.herokuapp.com',
           github: 'github.com/ansi6622/dragonjiblets',
-          image_link: 'http://media.thecelebrityauction.co/picture/c/0c/CwMdBE5KXRIWEBcRB00ZEBAVDB4KCwYFFloAGARbBAoeHBUMHhwrBQRGRVpXQUdXVUJdWlJDXURNXEJCXVBTSh0OEEYcABECOggC/FQBbChoIExFLGAMCPBUAGkwTDAYdExMHDBUXFlsCBR0dFwkXLFNQR0pAW0FZRUddQ0dLCQIC.jpg'
+          image_link: 'http://www.placecage.com/gif/199/301'
         }),
         knex('projects').insert({
           name: 'GeoCatch',
           description: 'Geolocation app that uses the IP Lookup API to identify hacker space locations and plot server coordinates on cesium.js globe.',
           url: 'site-globe.herokuapp.com',
           github: 'github.com/ansi6622/site-globe',
-          image_link: 'http://media.thecelebrityauction.co/picture/c/0c/CwMdBE5KXRIWEBcRB00ZEBAVDB4KCwYFFloAGARbBAoeHBUMHhwrBQRGRVpXQUdXVUJdWlJDXURNXEJCXVBTSh0OEEYcABECOggC/FQBbChoIExFLGAMCPBUAGkwTDAYdExMHDBUXFlsCBR0dFwkXLFNQR0pAW0FZRUddQ0dLCQIC.jpg'
+          image_link: 'http://www.placecage.com/gif/200/302'
         }),
       ]);
     });
